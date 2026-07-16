eb61d3b8971097201750ac339f563e2c326c4e53
Sampath Bandaranayake <vfxsampath@gmail.com>
2026-07-16 11:13:34 +0900

    Initialize AI Algorithms Lab and add Logistic Regression example

---
+++ 01_classification/logistic_regression/src/evaluate.py
@@ -0,0 +1,1 @@
+"""Evaluate a trained Logistic Regression model. Already done in train.py, but this is a separate file for demonstration purposes."""
---
+++ 01_classification/logistic_regression/src/predict.py
@@ -0,0 +1,1 @@
+"""Make predictions using a trained Logistic Regression model. Already done in train.py, but this is a separate file for demonstration purposes."""
